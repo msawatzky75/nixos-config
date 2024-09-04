4897af17d183f07b6554c8c5440a9e4817c0cd50
Matthew Sawatzky <msawatzky75@gmail.com>
2024-09-04 18:50:05 -0500

    configure the network properly

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -10,10 +10,10 @@
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-  networking.interfaces.enp5s0f1.ipv4.addresses = [{
-    address = "172.16.0.2";
-    prefixLength = 16;
-  }];
+#  networking.interfaces.enp5s0f1.ipv4.addresses = [{
+#    address = "172.16.0.2";
+#    prefixLength = 16;
+#  }];
   networking.interfaces.enp5s0f0.ipv4.addresses = [{
     address = "172.16.0.2";
     prefixLength = 16;
