44c90b19ea71eb79d189f49d5b6c0bd98afffaf3
Matthew Sawatzky <msawatzky75@gmail.com>
2024-09-06 22:42:28 -0500

    update steam config

--- nixos/games/steam.nix
+++ nixos/games/steam.nix
@@ -1,11 +1,28 @@
 { pkgs, ... }:
 
 {
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
+  programs = {
+    gamescope = {
+      enable = true;
+      capSysNice = true;
+    };
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+      gamescopeSession.enable = true;
+    };
   };
+
+  hardware.xone.enable = true;
   hardware.graphics.enable32Bit = true;
   hardware.pulseaudio.support32Bit = true;
+
+  services.getty.autologinUser = "matthew";
+  environment = {
+    systemPackages = [pkgs.mangohud];
+    loginShellInit = ''
+      [[ "$(tty)" = "/dev/tty1" ]] && ./gs.sh
+    '';
+  };
 }
