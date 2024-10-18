05253ec73b196d7cdedfc193f851e6a4fc1b9135
Matthew Sawatzky <msawatzky75@gmail.com>
2024-10-18 01:30:15 -0500

    small updates

--- nixos/applications/folding.nix
+++ nixos/applications/folding.nix
@@ -1,13 +1,16 @@
 { pkgs, ... }:
 {
   nixpkgs.config.allowUnfree = true;
-  # hardware.amdgpu.opencl.enable = true;
-  # hardware.graphics.extraPackages = with pkgs; [
-  #   rocmPackages.clr.icd
-  # ];
+  hardware.amdgpu.opencl.enable = true;
+  hardware.graphics.extraPackages = with pkgs; [
+    rocmPackages.clr.icd
+  ];
   nixpkgs.config.rocmSupport = true;
   environment.systemPackages = with pkgs; [
     clinfo
+    rocmPackages.rocblas
+    rocmPackages.hipblas
+    rocmPackages.clr
   ];
   services.foldingathome = {
     enable = true;
